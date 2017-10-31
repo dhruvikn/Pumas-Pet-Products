02e8a3a937f29c614255c392b5f1c93a130621bc
Dhruvik Neharia <dhruvikn@gmail.com>
2017-10-31 22:27:47 +0530

    Few tweaks.

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'pumas');
+define('DB_NAME', 'dhruvikn_pumas');
 
 /** MySQL database username */
 define('DB_USER', 'root');
